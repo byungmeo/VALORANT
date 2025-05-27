98a67db0e302a0b69d507aca47e12e4f6ae67f8f
chungheonLee <fire6376@gmail.com>
2025-05-27 20:08:08 +0900

    feat : kayo 스킬 투사체와 동작 연결 - ToDo : 실제 동작 구현(제압, 데미지) / 애니메이션 데이터 삽입

--- UnrealEngine/Valorant/Source/Valorant/AbilitySystem/Abilities/KAYO/KAYO_E_ZEROPOINT.h
+++ UnrealEngine/Valorant/Source/Valorant/AbilitySystem/Abilities/KAYO/KAYO_E_ZEROPOINT.h
@@ -1,14 +1,9 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "AbilitySystem/Abilities/BaseGameplayAbility.h"
 #include "KAYO_E_ZEROPOINT.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class VALORANT_API UKAYO_E_ZEROPOINT : public UBaseGameplayAbility
 {
@@ -16,4 +11,15 @@ class VALORANT_API UKAYO_E_ZEROPOINT : public UBaseGameplayAbility
 
 public:
 	UKAYO_E_ZEROPOINT();
+
+protected:
+	// 어빌리티 준비 단계
+	virtual void PrepareAbility() override;
+	
+	// 후속 입력 처리 (좌클릭으로 나이프 던지기)
+	virtual bool OnLeftClickInput() override;
+	
+	// 억제 나이프 던지기 실행
+	UFUNCTION(BlueprintCallable, Category = "Ability")
+	bool ThrowKnife();
 };
--- UnrealEngine/Valorant/Source/Valorant/AbilitySystem/Abilities/KAYO/KAYO_Q_FLASHDRIVE.cpp
+++ UnrealEngine/Valorant/Source/Valorant/AbilitySystem/Abilities/KAYO/KAYO_Q_FLASHDRIVE.cpp
@@ -1,8 +1,7 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "KAYO_Q_FLASHDRIVE.h"
+﻿#include "KAYO_Q_FLASHDRIVE.h"
 #include "AbilitySystem/ValorantGameplayTags.h"
+#include "AgentAbility/KayO/Flashbang.h"
+#include "GameFramework/Character.h"
 
 UKAYO_Q_FLASHDRIVE::UKAYO_Q_FLASHDRIVE(): UBaseGameplayAbility()
 {
@@ -10,4 +9,63 @@ UKAYO_Q_FLASHDRIVE::UKAYO_Q_FLASHDRIVE(): UBaseGameplayAbility()
 	Tags.AddTag(FGameplayTag::RequestGameplayTag(FName("Input.Skill.Q")));
 	SetAssetTags(Tags);
 	m_AbilityID = 3002;
+	
+	// FLASH/drive는 준비 후 좌클릭/우클릭으로 던지기 방식 선택
+	ActivationType = EAbilityActivationType::WithPrepare;
+	FollowUpInputType = EFollowUpInputType::LeftOrRight;
+}
+
+bool UKAYO_Q_FLASHDRIVE::OnLeftClickInput()
+{
+	// 좌클릭: 직선 던지기 (빠른 속도)
+	return ThrowFlashbang(false);
+}
+
+bool UKAYO_Q_FLASHDRIVE::OnRightClickInput()
+{
+	// 우클릭: 포물선 던지기 (느린 속도, 높은 궤도)
+	return ThrowFlashbang(true);
+}
+
+bool UKAYO_Q_FLASHDRIVE::ThrowFlashbang(bool bAltFire)
+{
+	if (!HasAuthority(&CurrentActivationInfo) || !ProjectileClass)
+	{
+		UE_LOG(LogTemp, Error, TEXT("KAYO Q - 권한 없음 또는 투사체 클래스 없음"));
+		return false;
+	}
+
+	ACharacter* Character = Cast<ACharacter>(GetAvatarActorFromActorInfo());
+	if (!Character)
+	{
+		UE_LOG(LogTemp, Error, TEXT("KAYO Q - 캐릭터 참조 실패"));
+		return false;
+	}
+
+	// 플래시뱅 스폰
+	FVector SpawnLocation = Character->GetActorLocation() + Character->GetActorForwardVector() * 100.0f;
+	FRotator SpawnRotation = Character->GetControlRotation();
+
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.Owner = Character;
+	SpawnParams.Instigator = Character;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+	AFlashbang* Flashbang = GetWorld()->SpawnActor<AFlashbang>(ProjectileClass, SpawnLocation, SpawnRotation, SpawnParams);
+	
+	if (Flashbang)
+	{
+		// 플래시뱅의 ActiveProjectileMovement 함수 호출 (bAltFire 파라미터로 던지기 방식 결정)
+		Flashbang->ActiveProjectileMovement(bAltFire);
+		SpawnedProjectile = Flashbang;
+		
+		UE_LOG(LogTemp, Warning, TEXT("KAYO Q - 플래시뱅 생성 성공 (AltFire: %s)"), 
+			bAltFire ? TEXT("true") : TEXT("false"));
+		return true;
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("KAYO Q - 플래시뱅 생성 실패"));
+		return false;
+	}
 }
