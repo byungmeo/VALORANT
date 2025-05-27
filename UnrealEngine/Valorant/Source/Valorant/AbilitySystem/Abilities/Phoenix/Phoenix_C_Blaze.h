190b3faa4b78f7845a54d776fde6999bb3e31122
chungheonLee <fire6376@gmail.com>
2025-05-27 21:51:37 +0900

    feat : 피닉스 C 구현 - ToDo : 벽 GE / 벽 overlap / 회전 메쉬 멀티 동기화

--- UnrealEngine/Valorant/Source/Valorant/AbilitySystem/Abilities/Phoenix/Phoenix_C_Blaze.h
+++ UnrealEngine/Valorant/Source/Valorant/AbilitySystem/Abilities/Phoenix/Phoenix_C_Blaze.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "AbilitySystem/Abilities/BaseGameplayAbility.h"
+#include "Phoenix_C_BlazeProjectile.h"
 #include "Phoenix_C_Blaze.generated.h"
 
 UCLASS()
@@ -8,5 +9,33 @@ class VALORANT_API UPhoenix_C_Blaze : public UBaseGameplayAbility
 {
     GENERATED_BODY()
 
+public:
     UPhoenix_C_Blaze();
-}; 
+
+protected:
+    // 벽 투사체 클래스
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Projectile")
+    TSubclassOf<class APhoenix_C_BlazeProjectile> BlazeProjectileClass;
+    
+    // 좌/우클릭별 다른 애니메이션
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Animation")
+    UAnimMontage* ExecuteStraightMontage_1P = nullptr;
+    
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Animation")
+    UAnimMontage* ExecuteStraightMontage_3P = nullptr;
+    
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Animation")
+    UAnimMontage* ExecuteCurvedMontage_1P = nullptr;
+    
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Animation")
+    UAnimMontage* ExecuteCurvedMontage_3P = nullptr;
+
+    // 오버라이드 함수들
+    virtual bool OnLeftClickInput() override;
+    virtual bool OnRightClickInput() override;
+    virtual void ExecuteAbility() override;
+    
+private:
+    // 벽 투사체 생성
+    bool SpawnBlazeProjectile(EBlazeMovement2Type MovementType);
+};
