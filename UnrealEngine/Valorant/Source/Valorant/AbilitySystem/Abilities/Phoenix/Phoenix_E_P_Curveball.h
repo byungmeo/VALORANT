06ea362b417684ac832b96582f1599e390df636d
chungheonLee <fire6376@gmail.com>
2025-05-28 14:17:17 +0900

    chore : 피닉스 E 투사체 수치조정

--- UnrealEngine/Valorant/Source/Valorant/AbilitySystem/Abilities/Phoenix/Phoenix_E_P_Curveball.h
+++ UnrealEngine/Valorant/Source/Valorant/AbilitySystem/Abilities/Phoenix/Phoenix_E_P_Curveball.h
@@ -21,18 +21,32 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	// 곡선 관련 설정
+	UPROPERTY(EditAnywhere, Category = "Curveball Settings")
+	float InitialSpeed = 2500.0f;
+	
+	// 폭발까지의 최대 시간
+	UPROPERTY(EditAnywhere, Category = "Curveball Settings")
+	float MaxAirTime = 1.5f;
+	
+	// 곡선이 시작되기까지의 지연 시간
+	UPROPERTY(EditAnywhere, Category = "Curveball Settings")
+	float CurveDelay = 0.15f;
+
+	// 최대 곡선 지속 시간
+	UPROPERTY(EditAnywhere, Category = "Curveball Settings")
+	float MaxCurveTime = 0.8f;
+
 private:
-	bool bShouldCurveRight;
-		
 	UPROPERTY(EditDefaultsOnly)
 	UStaticMeshComponent* CurveballMesh;
-	float InitialSpeed = 2500.f;
-	float MaxAirTime = 2.0f;
-	float TimeSinceSpawn = 0.f;
-	FRotator SpinRate = FRotator(0, 720, 360);
-	float CurveDelay = 0.15f;
+	// 상태 변수
+	bool bShouldCurveRight = true;  // true: 오른쪽, false: 왼쪽
+	float CurrentCurveTime = 0.0f;
 	bool bHasStartedCurving = false;
-	float CurrentCurveTime = 0.f;
-	float MaxCurveTime = 1.2f;
+	float TimeSinceSpawn = 0.0f;
+
+	FRotator SpinRate = FRotator(0, 720, 0);
+
 	float CurveStrength = 1200.f;
 };
