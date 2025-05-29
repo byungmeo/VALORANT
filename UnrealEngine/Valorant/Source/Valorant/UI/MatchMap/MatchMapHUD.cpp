e09d974e97d28bafa9695de8f6ed79eb7b51b208
heenchon <heenchon@naver.com>
2025-05-29 16:17:12 +0900

    chore: HUD 자잘한 수정

--- UnrealEngine/Valorant/Source/Valorant/UI/MatchMap/MatchMapHUD.cpp
+++ UnrealEngine/Valorant/Source/Valorant/UI/MatchMap/MatchMapHUD.cpp
@@ -204,6 +204,7 @@ void UMatchMapHUD::DisplayAnnouncement(EMatchAnnouncement MatchAnnouncement, flo
 
 	Overlay_Timer->SetVisibility(ESlateVisibility::Visible);
 	Img_Spike->SetVisibility(ESlateVisibility::Hidden);
+	OnLowState(false);
 	
 	GetWorld()->GetTimerManager().SetTimer(AnnouncementTimerHandle, this, &UMatchMapHUD::HideAnnouncement, DisplayTime, false);
 	bIsPreRound = true;
